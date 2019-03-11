b6ed0cab61633144c92d0031734f69a455cc273c
Evan <ImEvanWay@126.com>
2019-03-11 19:44:01 +0800

    修改

--- src/test/java/com/soecode/lyf/BookTest.java
+++ src/test/java/com/soecode/lyf/BookTest.java
@@ -9,7 +9,7 @@ public class BookTest {
 
     @Test
     public void bookTest() {
-    	//使用ClassPathXmlApplicationContext加载bean
+    	//使用ClassPathXmlApplicationContext加载bean 
         ApplicationContext ac = new ClassPathXmlApplicationContext(new String[] {"classpath:spring/spring-service.xml","classpath:spring/spring-dao.xml"});
         Book book = (Book) ac.getBean("test");
         book.setBookId(666);
