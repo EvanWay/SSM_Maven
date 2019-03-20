ac8c7f8ceba413dc7a7df632082dbe181ec7724f
Evan <ImEvanWay@126.com>
2019-03-20 20:17:08 +0800

    接口继续写

--- src/main/java/com/evanway/seckill/web/SeckillController.java
+++ src/main/java/com/evanway/seckill/web/SeckillController.java
@@ -1,15 +1,23 @@
 package com.evanway.seckill.web;
 
+import java.nio.channels.NonReadableChannelException;
 import java.util.List;
+
+import javax.xml.ws.RespectBinding;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.evanway.seckill.dto.Exposer;
+import com.evanway.seckill.dto.SeckillExecution;
+import com.evanway.seckill.dto.SeckillResult;
 import com.evanway.seckill.entity.Seckill;
 import com.evanway.seckill.service.SeckillService;
 
@@ -27,5 +35,48 @@ public class SeckillController {
 		model.addAttribute("list", list);
 		// list.jsp + model = ModelAndView
 		return "list";// WEB-INF/jsp/'list'.jsp
+	}
+
+	@RequestMapping(value = "/{seckillId}/detail", method = RequestMethod.GET)
+	public String detail(@PathVariable("seckillId") Long seckillId, Model model) {
+		if (seckillId == null) {
+			return "redirect:/seckill/list";
+		}
+		Seckill seckill = seckillService.getById(seckillId);
+		if (seckill == null) {
+			return "forward:/seckill/list";
+		}
+		model.addAttribute("seckill", seckill);
+		return "detail";
+	}
+
+	@RequestMapping(value = "/{seckillId}/exposer", method = RequestMethod.POST, produces = {
+			"application/json;charset=UTF-8" })
+	@ResponseBody
+	public SeckillResult<Exposer> exposer(Long seckillId) {
+		SeckillResult<Exposer> result;
+		try {
+			Exposer exposer = seckillService.exportSeckillUrl(seckillId);
+			result = new SeckillResult<Exposer>(true, exposer);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			result = new SeckillResult<Exposer>(false, e.getMessage());
+		}
+		return result;
+	}
+
+	@RequestMapping(value = "/{seckillId}/{md5}/execution", method = RequestMethod.POST, produces = {
+			"application/json;charset=UTF-8" })
+	@ResponseBody
+	public SeckillResult<SeckillExecution> excute(Long seckillId, Long userPhone, String md5) {
+		SeckillResult<SeckillExecution> result;
+		try {
+			SeckillExecution seckillExecution = seckillService.executeSeckill(seckillId, userPhone, md5);
+			result = new SeckillResult<SeckillExecution>(true, seckillExecution);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			result = new SeckillResult<SeckillExecution>(false, e.getMessage());
+		}
+		return result;
 	}
 }
