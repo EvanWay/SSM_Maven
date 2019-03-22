d3749b45945f886f6a0b19c282b2d48b9ac45820
Evan <ImEvanWay@126.com>
2019-03-22 20:46:28 +0800

    redisdao test

---
+++ src/test/java/com/evanway/seckill/mapper/dao/cache/RedisDaoTest.java
@@ -0,0 +1,20 @@
+package com.evanway.seckill.mapper.dao.cache;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.evanway.seckill.BaseTest;
+
+public class RedisDaoTest extends BaseTest{
+
+	@Test
+	public void testGetSeckill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutSeckill() {
+		fail("Not yet implemented");
+	}
+}
